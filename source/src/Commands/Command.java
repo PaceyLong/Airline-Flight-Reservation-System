2a2efa3850ee187202a1c3e24a74bdecedb40f71
jehling <jehling117@gmail.com>
2018-03-02 11:55:20 -0500

    Skeletal commit for Command Pattern structure

---
+++ source/src/Commands/Command.java
@@ -0,0 +1,8 @@
+package Commands;
+
+/**
+ * Top level abstract Command class for Command Pattern
+ */
+public interface Command {
+    public void execute();
+}
---
+++ source/src/Commands/RetrieveReservation.java
@@ -0,0 +1,8 @@
+package Commands;
+
+public class RetrieveReservation implements Command{
+    @Override
+    public void execute() {
+        // todo
+    }
+}
