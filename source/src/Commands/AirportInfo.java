4032cbf0804f1fcd49b2cecb596947a2eadbe6c9
jehling <jehling117@gmail.com>
2018-03-02 12:20:49 -0500

    AirportInfo comments

--- source/src/Commands/AirportInfo.java
+++ source/src/Commands/AirportInfo.java
@@ -2,6 +2,13 @@ package Commands;
 
 import java.util.ArrayList;
 
+/**
+ * Command block that handles logic required for Airport Info Request
+ *
+ * Legend: {keyword}, data
+ * Airport information request: {airport},airport;
+ *     airport is the three-letter code for the airport whose information is requested
+ */
 public class AirportInfo implements Command{
     @Override
     public void execute(ArrayList<String> input) {
