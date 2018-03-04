3fc02c21b9911b266516fb2a24e2c27d4d21b54b
jehling <jehling117@gmail.com>
2018-03-04 16:14:39 -0500

    Implemented singleton pattern for FlightsDB.java

--- source/src/TTARouteNetwork/FlightsDB.java
+++ source/src/TTARouteNetwork/FlightsDB.java
@@ -1,7 +1,30 @@
 package TTARouteNetwork;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
-// to be implemented
+/**
+ * @author Joshua Ehling
+ * Enforces Singleton Pattern
+ * Database used to map Origin Airport --> ArrayList <Flights>
+ */
 public class FlightsDB {
-    
+    /* attributes */
+    private HashMap<String, ArrayList<Flight>> flightsHashMap;
+
+    /* Enforce Singleton Pattern */
+    private static FlightsDB instance;
+
+    /* Singleton Pattern Accessor */
+    public static FlightsDB getInstance(){
+        if(instance == null) instance = new FlightsDB();
+        return instance;
+    }
+
+    /**
+     * Constructor
+     */
+    private FlightsDB(){
+        flightsHashMap = new HashMap<>();
+    }
 }
