bdaca247d78dee2dd0e694b1d4133bcb769d73ac
pxl2655 <pxl2655@rit.edu>
2018-03-03 22:00:16 -0500

    update reservation, flight, and itinerary, and its comments

--- source/src/Itinerary/Itinerary.java
+++ source/src/Itinerary/Itinerary.java
@@ -12,31 +12,73 @@ public class Itinerary{
     private int numberFlightIn = 0;
     private Flight flight;
 
+    /**
+     * itinerary constructor
+     *
+     * @param totalPrice total price of the airfare
+     * @param numberFlightIn number of flight in itinerary
+     * @param fl flight
+     */
     public Itinerary(int totalPrice, int numberFlightIn, Flight fl) {
         this.totalPrice = totalPrice;
         this.numberFlightIn = numberFlightIn;
         this.flight = fl;
     }
 
+    /**
+     * Calculates total airfare for the Itinerary.
+     * create a flight list.
+     * increase number of flight in itinerary.
+     * @param fl
+     */
     public void addFlight(src.TTARouteNetwork.Flight fl){
         flightList.add(fl);
         totalPrice += fl.getAirfare();
         numberFlightIn += 1;
     }
 
+    /**
+     * totalPrice getter
+     * @return
+     */
     public int getTotalPrice(){
         return totalPrice;
     }
 
+    /**
+     * number of flight in itinerary getter
+     * @return
+     */
     public int getNumberFlightIn(){
         return numberFlightIn;
     }
 
+    /**
+     * Returns the 3-letter code representing the origin airport of the first flight in the itinerary.
+     * @return
+     */
+    public String getOrigin(){
+        return flightList.get(0).getOriginAirport();
+    }
+
+    /**
+     * Returns the 3-letter code representing the destination airport of the last flight in the itinerary.
+     * @return
+     */
+    public String getDestination(){
+        return flightList.get(flightList.size()-1).getDestinationAirport();
+    }
+
+    /**
+     * toString(): total price, number of flight in itinerary, flight number, origin, departure time,
+     * destination, arrival time.
+     * @return
+     */
     @Override
     public String toString(){
         return getTotalPrice() + "," + getNumberFlightIn() + "[,"
-                + flight.getFlightNumber() + "," + flight.getOriginAirport() + ","
-                + flight.getDepatureTime() + "," + flight.getDestinationAirport()
+                + flight.getFlightNumber() + "," + getOrigin() + ","
+                + flight.getDepatureTime() + "," + getDestination()
                 + "," + flight.getArrivalTime();
     }
 }
--- source/src/Itinerary/Reservation.java
+++ source/src/Itinerary/Reservation.java
@@ -1,13 +1,99 @@
 package src.Itinerary;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class Reservation {
     private String passengerName;
-    HashMap<String, src.Itinerary.Itinerary> reservation = new HashMap<String, src.Itinerary.Itinerary>();
+    private Itinerary itinerary;
+    private String origin;
+    private String destination;
+    private int numberOfItinerary = 0;
+    HashMap<String, src.Itinerary.Itinerary> reservationList = new HashMap<String, src.Itinerary.Itinerary>();
 
+    /**
+     * reservation constructor
+     *
+     * @param passengerName passenger name
+     * @param itinerary itinerary
+     * @param origin origin airport
+     * @param destination destination airport
+     */
+    public Reservation(String passengerName, Itinerary itinerary, String origin, String destination) {
+        this.passengerName = passengerName;
+        this.itinerary = itinerary;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    /**
+     * Retrieves the passenger's name who created the reservation.
+     * @return passenger name
+     */
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    /**
+     * Retrieves the itinerary contained within the reservation.
+     * @return itinerary the reservation was made for
+     */
+    public Itinerary getItinerary(){
+        return itinerary;
+    }
+
+    /**
+     * Reservation maintains the passenger's name, details of the reserved itinerary including:
+     * the price for the intinerary, flight information
+     * (flight number, origin and destination airports, departure and arrival times)
+     * for each flight in the itinerary.
+     *
+     * increase number of itinerary;
+     *
+     * @param itinerarys
+     */
+    public void addItinerary(Itinerary itinerarys){
+        reservationList.put(passengerName,itinerarys);
+        numberOfItinerary += 1;
+    }
+
+    /**
+     * Retrieves the origin airport for the reservation's itinerary.
+     * @return 3-letter airport code for origin airport
+     */
+    public String getOrigin(){
+        return itinerary.getOrigin();
+    }
+
+    /**
+     * Retrieves the destination airport for the reservation's itinerary.
+     * @return 3-letter airport code for destination airport
+     */
+    public String getDestination(){
+        return itinerary.getDestination();
+    }
+
+    /**
+     * Checks if a given passenger name, origin code, and destination code match the current reservation.
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reservation)) return false;
+        Reservation that = (Reservation) o;
+        return Objects.equals(passengerName, that.getPassengerName()) &&
+                Objects.equals(origin, that.getOrigin()) &&
+                Objects.equals(destination, that.getDestination());
+    }
+
+    /**
+     * toString(): # itineraries, itinerary.toString()
+     * @return
+     */
+    @Override
+    public String toString() {
+        return numberOfItinerary + ",\n" + itinerary.toString();
+    }
 }
