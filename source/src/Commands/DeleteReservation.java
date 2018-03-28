f85e2e209a521cbf3f18c231e748398c843a0618
jehling <jehling117@gmail.com>
2018-03-27 20:54:32 -0400

    Undo DeleteReservation implemented

--- source/src/Commands/DeleteReservation.java
+++ source/src/Commands/DeleteReservation.java
@@ -14,21 +14,29 @@ import java.util.ArrayList;
  *      origin: three-letter code for the reservation's origin airport
  *      destination: three-letter code for the reservation's destination airport
  */
-public class DeleteReservation extends Command {
-    /* attributes */
+public class DeleteReservation extends UndoableCommand {
+    /* Constants */
     private static final int DELETE_KEYWORD = 0;
     private static final int PASSENGER = 1;
     private static final int ORIGIN = 2;
     private static final int DESTINATION = 3;
+    private ReservationsDB reservationsDB = ReservationsDB.getInstance();
+
+    /* Attributes */
+    private Itinerary deletedItinerary;
 
     public DeleteReservation(ArrayList<String> input){
         super(input);
     }
 
     @Override
+    public void undo() {
+        reservationsDB.reserveItinerary(deletedItinerary, input.get(PASSENGER));
+    }
+
+    @Override
     public void execute() {
-        ReservationsDB reservationsDB = ReservationsDB.getInstance();
-        Itinerary itinerary = reservationsDB.getItinerary(input.get(PASSENGER), input.get(ORIGIN), input.get(DESTINATION));
-        reservationsDB.deleteItinerary(input.get(PASSENGER), itinerary);
+        deletedItinerary = reservationsDB.getItinerary(input.get(PASSENGER), input.get(ORIGIN), input.get(DESTINATION));
+        reservationsDB.deleteItinerary(input.get(PASSENGER), deletedItinerary);
     }
 }
